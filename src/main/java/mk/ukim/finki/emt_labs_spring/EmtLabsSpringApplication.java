105b63a3f96aec44f547005a73573cfe586d3a3b
Kiril Blazevski <blazevskikiril@hotmail.com>
2023-03-18 21:55:03 +0100

    initial commit

---
+++ src/main/java/mk/ukim/finki/emt_labs_spring/EmtLabsSpringApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt_labs_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtLabsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmtLabsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/emt_labs_spring/EmtLabsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt_labs_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtLabsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
